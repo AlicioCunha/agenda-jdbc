3e9326ae4f43454c95c62854a28beb4fedb25d3e
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-07 21:07:14 -0300

    implementando conexao com mysql

--- src/main/java/br/com/agendajdbc/data/ConexaoMysqlJDBC.java
+++ src/main/java/br/com/agendajdbc/data/ConexaoMysqlJDBC.java
@@ -1,2 +1,27 @@
-package br.com.agendajdbc.data;public class ConexaoMysqlJDBC {
+package br.com.agendajdbc.data;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConexaoMysqlJDBC implements ConexaoJDBC {
+
+    @Override
+    public Connection getConnection() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void commit() throws SQLException {
+
+    }
+
+    @Override
+    public void rollback() {
+
+    }
 }
