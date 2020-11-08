2f1fa99ddbd67b6fc0349776994a08a86754708e
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-07 21:35:20 -0300

    Classe de usuarioDAO e metodo de listar

--- src/main/java/br/com/agendajdbc/dao/UsuarioDAO.java
+++ src/main/java/br/com/agendajdbc/dao/UsuarioDAO.java
@@ -2,9 +2,13 @@ package br.com.agendajdbc.dao;
 
 import br.com.agendajdbc.data.ConexaoJDBC;
 import br.com.agendajdbc.data.ConexaoMysqlJDBC;
+import br.com.agendajdbc.model.Usuario;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
 
@@ -25,6 +29,27 @@ public class UsuarioDAO {
             this.conexao.commit();
         } catch (SQLException exception) {
             this.conexao.rollback();
+            throw exception;
+        }
+    }
+
+    public List<Usuario> listar() throws SQLException, ClassNotFoundException {
+        String sqlQuery = "SELECT * FROM usuario order by id";
+
+        try {
+            PreparedStatement statement = this.conexao.getConnection().prepareStatement(sqlQuery);
+            ResultSet resultSet = statement.executeQuery();
+
+            List<Usuario> usuarios = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Usuario usuario = new Usuario(resultSet.getLong("id"), resultSet.getString("nome"));
+                usuarios.add(usuario);
+            }
+
+            return usuarios;
+
+        } catch (SQLException exception) {
             throw exception;
         }
     }
